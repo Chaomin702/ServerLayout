10d1d7ab413cfc3639ea40bb7cc2d99f1dc2e7ae
Chaomin <chaomin702@gmail.com>
2017-03-05 09:57:43 +0800

    select nodes with large bw

--- ServerLayout/cdn.cpp
+++ ServerLayout/cdn.cpp
@@ -32,6 +32,7 @@ int main(int argc, char *argv[])
 
 	Solution so;
 	so.constructGraph(topo_file);
+	so.informationCollected();
     print_time("End");
 
 	return 0;
--- ServerLayout/optialLayout.cpp
+++ ServerLayout/optialLayout.cpp
@@ -6,6 +6,7 @@
 #include <boost/graph/edmonds_karp_max_flow.hpp>  
 #include <boost/graph/read_dimacs.hpp>  
 #include <boost/graph/graph_utility.hpp>  
+#include <assert.h>
 #include "optialLayout.h"
 using std::cout;
 using std::endl;
@@ -54,46 +55,73 @@ int Solution::importGraphFromFile(const string& filename) {
 	}
 	std::getline(infile, buf);
 	cout << "There are " << num_vertices(graph_) << " vertexes, " << num_edges(graph_) << " edges.\n";
+	//add consumer nodes
 	for (int i = 0; i < numConsumer; ++i) {
+		Vertex t = num_vertices(graph_);
 		std::getline(infile, buf);
 		std::stringstream ss(buf);
 		ss >> u >> v >> cap;
-		BwRequirements[u] = cap;
+		AddEdge(v, t, rev, cap, 0, graph_);
+		BwRequirements[t] = cap;
 	}
 	return 0;
 }
 
-
-//int main(int argc, char* argv[])
-//{
-//	Graph g;
-//	property_map<Graph, edge_capacity_t>::type
-//		capacity = get(edge_capacity, g);
-//	property_map<Graph, edge_reverse_t>::type
-//		rev = get(edge_reverse, g);
-//	property_map<Graph, edge_residual_capacity_t>::type
-//		residual_capacity = get(edge_residual_capacity, g);
-//
-//	//boost::add_edge(u, v, g);  
-//	AddEdge(0, 1, rev, 4, g);
-//	AddEdge(0, 2, rev, 9, g);
-//	AddEdge(1, 3, rev, 8, g);
-//	AddEdge(2, 3, rev, 6, g);
-//
-//
-//	//double flow = boost::boykov_kolmogorov_max_flow(g, u, x);  
-//	//double flow = boost::push_relabel_max_flow(g, u, x);  
-//
-//	long flow = push_relabel_max_flow(g, 0, 3);
-//	std::cout << "c  The total flow:" << std::endl;
-//	std::cout << "s " << flow << std::endl << std::endl;
-//	boost::graph_traits < Graph >::vertex_iterator u_iter, u_end;
-//	boost::graph_traits<Graph>::out_edge_iterator ei, e_end;
-//	for (boost::tie(u_iter, u_end) = vertices(g); u_iter != u_end; ++u_iter)
-//		for (boost::tie(ei, e_end) = out_edges(*u_iter, g); ei != e_end; ++ei)
-//			if (capacity[*ei] > 0)
-//				std::cout << "f " << *u_iter << " " << target(*ei, g) << " "
-//				<< (capacity[*ei] - residual_capacity[*ei]) << std::endl;
-//
-//	return 0;
-//}
+void Solution::informationCollected() {
+	int bwSum = 0;
+	for (auto i : BwRequirements)
+		bwSum += i.second;
+	cout << "bandwidth requirement sum: " << bwSum << endl;
+	//sort nodes by bandwidth
+	typedef pair<Vertex, int> flows;
+	struct comp {
+		bool operator()(const flows& lhs, const flows& rhs) { return lhs.second > rhs.second; };
+	};
+	std::set<flows, comp> S;
+	for (auto vi = vertices(graph_).first; vi != vertices(graph_).second; ++vi) {
+		if (BwRequirements.find(*vi) != BwRequirements.end()) continue;
+		int flowSum = 0;
+		for (auto ei = out_edges(*vi, graph_).first; ei != out_edges(*vi, graph_).second; ++ei) {
+			flowSum += get(edge_capacity, graph_, *ei);
+		}
+		S.insert({ *vi,flowSum });
+	}
+	//select nodes with large bandwidth
+	cout << "\nselect nodes with large bandwidth:\nnode	bandwidth" << endl;
+	vector<Vertex> center;
+	int curFlow = 0;
+	for (auto i : S) {
+		cout << i.first << "	" << i.second << endl;
+		curFlow += i.second;
+		center.push_back(i.first);
+		if (curFlow > bwSum * 4) break;
+	}
+	//add super start point and super end point
+	Vertex s = num_vertices(graph_), t = s + 1;
+	property_map<Graph, edge_capacity_t>::type
+		capacity = get(edge_capacity, graph_);
+	property_map<Graph, edge_reverse_t>::type
+		rev = get(edge_reverse, graph_);
+	property_map<Graph, edge_residual_capacity_t>::type
+		residual_capacity = get(edge_residual_capacity, graph_);
+	for (auto u : center)
+		AddEdge(s, u, rev, INF_CAPACITY, 0, graph_);
+	for (auto u : BwRequirements)
+		AddEdge(u.first, t, rev, INF_CAPACITY, 0, graph_);
+	//maxflow 
+	long flow = push_relabel_max_flow(graph_, s, t);
+	//cout flow of comsumer
+	cout << "source	target	flow	requirement" << endl;
+	for (auto i : BwRequirements) {
+		Vertex v = i.first;
+		auto ei = out_edges(v, graph_).first;
+		Vertex u = target(*ei, graph_);
+		Edge e = edge(u, v, graph_).first;
+		//edge must exist
+		assert(edge(u, v, graph_).second);
+		std::cout << "f " << u << "->" << v << "	"
+			<< (capacity[e] - residual_capacity[e]) << "	" << BwRequirements[v] << std::endl;
+	}
+	cout << "c  The total flow:" << endl;
+	cout << "s " << flow << endl;
+}
